3dfe7446975eb4cacb33044b300a227466267516
Chanda (Cat) Grayson <chandagrayson@gmail.com>
2018-11-05 02:12:34 +0000

    automatically backed up by learn

---
+++ Badges.rb
@@ -0,0 +1,21 @@
+def badge_maker(name)
+    
+  puts "Hello, my name is #{name}."
+  
+  name[ " Edsger ", " Ada " , " Charles ", " Alan ", " Grace " , " Linus ", " Matz " ]
+  
+end
+  
+def batch_badge_creator(name)
+  
+  number[ " 1 " , " 2 " , " 3 " , " 4 " , " 5 " , " 6 " , " 7 " ].each |name| + 1
+  
+end
+  
+def assign_rooms(number)
+  
+  name (number + 1)
+  
+  puts "Hello #{name}! You'll be assigned to room #{number}!"
+  
+end
